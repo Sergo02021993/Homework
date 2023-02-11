e46576a7a8389aa23caa08e9811dd4a9070b223d
Sergey <sivak.93@bk.ru>
2023-02-11 21:30:49 +0500

    Work3

---
+++ Новая папка/Homework C#/Work3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите значение: ");
+string inputValue = Console.ReadLine();
+
+int value = Convert.ToInt32(inputValue);
+
+if (value % 2 == 0)
+{
+    Console.WriteLine($"Число {value} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {value} не является четным");
+}
